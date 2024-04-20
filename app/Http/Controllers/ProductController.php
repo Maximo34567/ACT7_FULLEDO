77165d3efa3f6bd9d00d101660de467a06253622
Maximo34567 <markangelofulledo@gmail.com>
2024-04-20 23:54:37 +0800

    act7_fulledo

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = "Product list form in productcontroller";
+        return view( 'products.index', ['products' => $products]);
+    }
+
+    public function create() {
+        return view ('products.create');
+    }
+}
